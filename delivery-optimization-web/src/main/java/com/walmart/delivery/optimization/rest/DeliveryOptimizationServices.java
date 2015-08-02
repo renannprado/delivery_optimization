93df512268853ad10ae32cfdbca0427f5340a8c5
Renann Prado <prado.renann@gmail.com>
2015-08-01 21:50:13 -0700

    RESTful webservice to add logistics network fully working.

--- delivery-optimization-web/src/main/java/com/walmart/delivery/optimization/rest/DeliveryOptimizationServices.java
+++ delivery-optimization-web/src/main/java/com/walmart/delivery/optimization/rest/DeliveryOptimizationServices.java
@@ -1,8 +1,12 @@
 package com.walmart.delivery.optimization.rest;
 
-import javax.enterprise.context.ApplicationScoped;
+import com.walmart.delivery.optimization.beans.EntityMapFacadeLocal;
+import com.walmart.delivery.optimization.entities.EntityMap;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,15 +16,25 @@ import javax.ws.rs.core.MediaType;
  * @author renannp
  */
 @Path(value = "/")
-@ApplicationScoped //make sure that all resources will be discarded after the call to these services
-public class DeliveryOptimizationServices 
-{    
-    @GET
+public class DeliveryOptimizationServices implements Serializable
+{
+    @EJB
+    EntityMapFacadeLocal entityMapFacade;    
+    
+    @PUT
     @Path(value = "/addLogisticsNetwork")
     @Produces(value = MediaType.APPLICATION_JSON)
     @Consumes(value = MediaType.APPLICATION_JSON)
-    public String registerLogisticsNetwork()
+    public EntityMap registerLogisticsNetwork(@Valid EntityMap newMapRequest)
     {
-        return "Success";
+        // set the map object to the logistics network objects so they can reference the map during persist
+        newMapRequest.getLogisticsNetwork().stream().forEach((n) -> {
+            n.setEntityMap(newMapRequest);
+        });
+        
+        // persist the new map in the database
+        this.entityMapFacade.create(newMapRequest);
+
+        return newMapRequest;
     }
 }
